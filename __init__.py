35ea22e776f3a80d06f7b85ff9e5b8f0ce1b0d13
vladsaveliev <vladsaveliev@me.com>
2016-06-17 20:59:16 +0300

    Add __init__.py to import location

---
+++ __init__.py
@@ -0,0 +1,3 @@
+SUPPORTED_GENOMES = ['hg19', 'hg19-noalt', 'hg38', 'hg38-noalt', 'hg19-chr20']
+ANNOTATION = 'RefSeq/RefSeq_CDS_miRNA.all_features.{genome}.canon.bed'
+CDS = 'RefSeq/RefSeq_CDS.{genome}.bed'
