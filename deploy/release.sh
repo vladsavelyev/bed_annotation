8cf9cf642992f7e4bf4e66434791af45082ffc4c
vladsaveliev <vladislav.sav@gmail.com>
2018-08-23 11:36:15 +1000

    Deploy fixes

--- deploy/release.sh
+++ deploy/release.sh
@@ -2,12 +2,12 @@
 
 VERSION=$1
 if [ -z $VERSION ] ; then
-    echo 'Usage: $1 VERSION'  >&2
+    echo "Usage: $1 VERSION" >&2
     exit 1
 fi
 
 echo $VERSION > VERSION.txt
 git add VERSION.txt
-git commit -m 'Bump version $VERSION'
+git commit -m "Release $VERSION"
 git tag $VERSION
 git push --tags
