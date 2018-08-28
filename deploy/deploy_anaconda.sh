75bda6dd14f8be8be9ac8f0876409eccbee323db
vladsaveliev <vladislav.sav@gmail.com>
2018-08-28 14:35:57 +1000

    Deploy conda: fix PACKAGE_NAME var

--- deploy/deploy_anaconda.sh
+++ deploy/deploy_anaconda.sh
@@ -2,7 +2,7 @@
 # this script uses the ANACONDA_TOKEN env var. 
 # to create a token:
 # >>> anaconda login
-# >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
+# >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGE_NAME --scopes "api:write api:read"
 set -e
 set -x
 
@@ -17,10 +17,10 @@ if [ -z $ANACONDA_TOKEN ] ; then
 fi
 
 echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda/conda-bld/linux-64/${PACKAGENAME}-*.tar.bz2 --output-dir $HOME/miniconda/conda-bld/
+conda convert --platform all $HOME/miniconda/conda-bld/linux-64/${PACKAGE_NAME}-*.tar.bz2 --output-dir $HOME/miniconda/conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/**/${PACKAGENAME}-*.tar.bz2
+anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/**/${PACKAGE_NAME}-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
